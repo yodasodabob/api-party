9cf4c87d5a3b291df8b5dfa4774cf6521588614e
Laurence Ruberl <bknd225@gmail.com>
2017-05-30 13:20:12 -0400

    Sync with xtbc17s1 github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Switch, Route, NavLink} from 'react-router-dom'
+import { Switch, Route, NavLink } from 'react-router-dom'
 import Github from './Github'
+import Nasa from './Nasa'
+import Homework from './Homework'
 import './App.css';
 
 class App extends Component {
@@ -10,21 +12,28 @@ class App extends Component {
         <div className="App-header">
           <div className="App-headings">
             <h3>Ain't no party like an</h3>
-            <h1>API Party!</h1>
+            <h1>API Party</h1>
           </div>
           <ul className="nav-links">
             <li>
-              <NavLink to='/github'>Github API</NavLink>
+              <NavLink to={'/github'}>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/nasa'}>NASA API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/homework'}>Homework</NavLink>
             </li>
           </ul>
         </div>
         <Switch>
           <Route path='/github' component={Github} />
-          <Route render = {() => <p>To get started, click one of the links above</p>} />
+          <Route path='/nasa' component={Nasa} />
+          <Route path='/homework' component={Homework} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
     );
   }
 }
-
 export default App;
